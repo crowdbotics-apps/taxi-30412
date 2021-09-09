1794e28d3b98861207114f658f0c78a005576c68
Crowdbotics <team@crowdbotics.com>
2021-09-09 07:53:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "taxi_30412Identifier",
+  url: "https://taxi_30412Identifier.botics.co",
+  api: "https://taxi_30412Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
